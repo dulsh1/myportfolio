55e029d6ff6c17fa432b0a9f797f1712d575984f
supun dulshan <161543413+dulsh1@users.noreply.github.com>
2025-06-18 11:47:17 +0530

    Update About.jsx

--- supundulshan/src/components/About/About.jsx
+++ supundulshan/src/components/About/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactTypingEffect from 'react-typing-effect';
 import Tilt from 'react-parallax-tilt';
-import profileImage from '../../assets/supunulshan.png';
+//import profileImage from '../../assets/supunulshan.png';
 
 const About = () => {
   return (
@@ -74,7 +74,7 @@ const About = () => {
             gyroscope={true}
           >
             <img
-              src={profileImage}
+              //src={profileImage}
               alt="supun dulshan"
               className="w-full h-full rounded-full object-cover drop-shadow-[0_10px_20px_rgba(130,69,236,0.5)]"
             />
